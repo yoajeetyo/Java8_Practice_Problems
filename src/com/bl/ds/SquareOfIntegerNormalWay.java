482a308328044011b88b97cde2436f3fe5a67e57
yoajeetyo <sajeetkumar40@gmail.com>
2021-09-19 07:33:49 +0530

    Added : Square of number in normal way

---
+++ src/com/bl/ds/SquareOfIntegerNormalWay.java
@@ -0,0 +1,14 @@
+package com.bl.ds;
+
+public class SquareOfIntegerNormalWay {
+	
+	public int squareIt(int x) {
+		return x*x;
+	}
+
+public static void main(String[] args) {
+	SquareOfIntegerNormalWay squareOfIntegerNormalWay =new SquareOfIntegerNormalWay();
+	System.out.println("The square of 20 is : "+squareOfIntegerNormalWay.squareIt(20));
+	System.out.println("The square of 50 is : "+squareOfIntegerNormalWay.squareIt(50));
+}
+}
